3eebbebf546826c9d0af98c75dbc2e4f1a165616
Alec Sears <76724804+searsam1@users.noreply.github.com>
2022-06-16 07:51:43 -0600

    Update digitRoot.java

--- digitRoot/digitRoot.java
+++ digitRoot/digitRoot.java
@@ -1,28 +1,21 @@
-
-
-
+// https://edabit.com/challenge/
+// KgR2JhEuGD7onbMLx
 public class Root {
 	public static int digitRoot(long n) {
 		// https://www.javatpoint.com/java-long-to-string
 		String str = String.valueOf(n);
-
 		if (str.length() == 1){
 			return Integer.valueOf(str);
 		}
-
 		int sum = 0;
-		
 		for (int i=0; i<str.length(); i++){
 			char s = str.charAt(i);
 			sum += Character.getNumericValue(s);
 		}
-
-
-
 		return digitRoot(sum);
-
 	}
 	public static void main(String[] args) {
 		System.out.println(digitRoot(123339393333L));
 	}
 }
+//
